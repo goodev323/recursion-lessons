5e50cbaae681e3647f7241c4ef7155a5033901f8
go0dev <rhikat5323@gmail.com>
2021-10-11 14:41:59 -0400

    Commit source code for lesson 420 calculating order is O(n).

--- src/dev/goo/lesson420/Solution.java
+++ src/dev/goo/lesson420/Solution.java
@@ -1,28 +1,39 @@
 package dev.goo.lesson420;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+
 public class Solution {
     public static int largestRectangle(int[] h){
         int largestRectangle = 0;
-        for(int i = 0; i < h.length; i++) {
-            int front = i;
-            int back = i;
-            for(int j = i - 1; 0 <= j; j--) {
-                if(h[i] > h[j]) {
-                    break;
-                }
-                front = j;
-            }
-            for(int j = i + 1; j < h.length; j++) {
-                if(h[i] > h[j]) {
-                    break;
-                }
-                back = j;
-            }
-            int rectangle = (back - front + 1) * h[i];
-            if(largestRectangle < rectangle) {
-                largestRectangle = rectangle;
-            }
+        for(int i = 1; i <= h.length; i++) {
+            int[] arr = minWindowArrK(h, i);
+            int rectangle = Arrays.stream(arr).max().getAsInt() * i;
+            largestRectangle = Math.max(largestRectangle, rectangle);
         }
         return largestRectangle;
+    }
+
+    private static int[] minWindowArrK(int[] intArr, int k) {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        for (int i = 0; i < k; i++) {
+            while (!deque.isEmpty() && intArr[deque.peekLast()] >= intArr[i]) {
+                deque.pollLast();
+            }
+            deque.add(i);
+        }
+        int[] resultArr = new int[intArr.length - k + 1];
+        for (int i = k; i < intArr.length; i++) {
+            resultArr[i - k] = intArr[deque.peek()];
+            while (!deque.isEmpty() && deque.peek() <= i - k) {
+                deque.poll();
+            }
+            while (!deque.isEmpty() && intArr[deque.peekLast()] >= intArr[i]) {
+                deque.pollLast();
+            }
+            deque.add(i);
+        }
+        resultArr[intArr.length - k] = intArr[deque.peek()];
+        return resultArr;
     }
 }
