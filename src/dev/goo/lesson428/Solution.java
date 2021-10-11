03d1f3b9f233e5cd9a8918878156a5847aab9336
go0dev <rhikat5323@gmail.com>
2021-10-11 12:38:20 -0400

    Commit source code for lesson 428. https://recursionist.io/dashboard/course/3/lesson/428

--- src/dev/goo/lesson428/Solution.java
+++ src/dev/goo/lesson428/Solution.java
@@ -1,5 +1,31 @@
 package dev.goo.lesson428;
 
-public class Solution {
+import java.util.ArrayDeque;
 
+public class Solution {
+    public static int[] minWindowArrK(int[] intArr, int k) {
+        if (k == 1) {
+            return intArr;
+        }
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        for (int i = 0; i < k; i++) {
+            while (!deque.isEmpty() && intArr[deque.peek()] >= intArr[i]) {
+                deque.poll();
+            }
+            deque.add(i);
+        }
+        int[] resultArr = new int[intArr.length - k + 1];
+        for (int i = k; i < intArr.length; i++) {
+            resultArr[i - k] = intArr[deque.peek()];
+            while (!deque.isEmpty() && deque.peek() <= i - k) {
+                deque.poll();
+            }
+            while (!deque.isEmpty() && intArr[deque.peek()] >= intArr[i]) {
+                deque.poll();
+            }
+            deque.add(i);
+        }
+        resultArr[intArr.length - k] = intArr[deque.peek()];
+        return resultArr;
+    }
 }
---
+++ src/dev/goo/lesson428/SolutionTest.java
@@ -0,0 +1,20 @@
+package dev.goo.lesson428;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    @Test
+    void minWindowArrK() {
+        assertArrayEquals(new int[]{2,3,1,1,12,3,10}, Solution.minWindowArrK(new int[]{2,3,1,1,12,3,10}, 1));
+        assertArrayEquals(new int[]{1,1,1,1,3}, Solution.minWindowArrK(new int[]{2,3,1,1,12,3,10}, 3));
+        assertArrayEquals(new int[]{1,1,1,1}, Solution.minWindowArrK(new int[]{2,3,1,1,12,3,10}, 4));
+        assertArrayEquals(new int[]{3,2,2,2}, Solution.minWindowArrK(new int[]{3,9,10,2,4,5}, 3));
+        assertArrayEquals(new int[]{2,2}, Solution.minWindowArrK(new int[]{3,9,10,2,4,5}, 5));
+        assertArrayEquals(new int[]{30,20,20,20,30}, Solution.minWindowArrK(new int[]{30,50,60,20,30,64,80}, 3));
+        assertArrayEquals(new int[]{30,50,20,20,30,64}, Solution.minWindowArrK(new int[]{30,50,60,20,30,64,80}, 2));
+        assertArrayEquals(new int[]{5,5,24,24,23,23,23}, Solution.minWindowArrK(new int[]{24,5,67,60,24,64,23,536,345}, 3));
+    }
+}
